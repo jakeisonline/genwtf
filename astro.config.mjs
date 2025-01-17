4c56765c4a029da083dc819427f643925611af1c
Jake Holman <me@jakeisonline.com>
2025-01-17 21:56:32 +0000

    add all the dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import sitemap from '@astrojs/sitemap';
+
+import react from '@astrojs/react';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), react()],
+  adapter: vercel()
+});
